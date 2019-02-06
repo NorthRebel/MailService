3d3f7d76fc33884690856e61fb333532fe73d287
NorthRebel <zatinasever@gmail.com>
2019-02-06 14:03:12 +0300

    Fix response

--- MailService.API/Controllers/MailController.cs
+++ MailService.API/Controllers/MailController.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
-using MailService.API.Infrastructure;
+using System.Linq;
 using MailService.API.Models;
-using MailService.Domain.Entities;
-using MailService.Domain.Repositories;
-using MailService.Services.Mail;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MailService.Services.Mail;
+using System.Collections.Generic;
+using MailService.Domain.Entities;
+using MailService.API.Infrastructure;
+using MailService.Domain.Repositories;
 
 namespace MailService.API.Controllers
 {
@@ -91,9 +91,79 @@ namespace MailService.API.Controllers
 
             await _correspondenceRepository.UnitOfWork.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(Show), new { messageId }, "Hello!");
+            return CreatedAtAction(nameof(Show), new { messageId }, await GetSentMessageById(messageId));
         }
 
+
+        /// <summary>
+        /// Returns a list of all sent messages including correspondence result information 
+        /// </summary>
+        /// <returns>List of all sent messages</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<SentMessage>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> Show()
+        {
+            IEnumerable<Message> messages = await _messageRepository.GetAll();
+
+            var result = new List<SentMessage>();
+
+            foreach (Message message in messages)
+            {
+                SentMessage sentMessage = await GetSentMessageById(message.Id);
+                result.Add(sentMessage);
+            }
+
+            if (result.Any())
+                return Ok(result);
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Gets information about sent message including correspondence result
+        /// </summary>
+        /// <param name="messageId">Sent message id</param>
+        /// <returns>Information about sent message</returns>
+        [HttpGet("{messageId:int}")]
+        [ProducesResponseType(typeof(SentMessage), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> Show(int messageId)
+        {
+            SentMessage sentMessage = await GetSentMessageById(messageId);
+
+            if (sentMessage != null)
+                return Ok(sentMessage);
+
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Gets information about sent message including correspondence result
+        /// </summary>
+        /// <param name="messageId">Id of sent message</param>
+        /// <returns>Information about sent message</returns>
+        private async Task<SentMessage> GetSentMessageById(int messageId)
+        {
+            SentMessage result = null;
+
+            Message message = await _messageRepository.GetById(messageId);
+
+            if (message != null)
+            {
+                result = new SentMessage
+                {
+                    Id = message.Id,
+                    Subject = message.Subject,
+                    Body = message.Body,
+                    CreateDate = message.CreateDate
+                };
+
+                result.Correspondences = await GetCorrespondenceOfMessage(messageId);
+            }
+
+            return result;
+        }
 
         /// <summary>
         /// Saves the message to the database.
@@ -170,71 +240,7 @@ namespace MailService.API.Controllers
             }
 
             return correspondence;
-        }
-
-        /// <summary>
-        /// Returns a list of all sent messages including correspondence result information 
-        /// </summary>
-        /// <returns>List of all sent messages</returns>
-        [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<SentMessage>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        public async Task<IActionResult> Show()
-        {
-            IEnumerable<Message> messages = await _messageRepository.GetAll();
-
-            var result = new List<SentMessage>();
-
-            foreach (Message message in messages)
-            {
-                var sentMessage = new SentMessage
-                {
-                    Id = message.Id,
-                    Subject = message.Subject,
-                    Body = message.Body,
-                    CreateDate = message.CreateDate
-                };
-
-                sentMessage.Correspondences = await GetCorrespondenceOfMessage(message.Id);
-                
-                result.Add(sentMessage);
-            }
-            
-            if (result.Any())
-                return Ok(result);
-
-            return NoContent();
-        }
-
-        /// <summary>
-        /// Gets information about sent message including correspondence result
-        /// </summary>
-        /// <param name="messageId">Sent message id</param>
-        /// <returns>Information about sent message</returns>
-        [HttpGet("{messageId:int}")]
-        [ProducesResponseType(typeof(SentMessage), (int)HttpStatusCode.OK)]
-        [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        public async Task<IActionResult> Show(int messageId)
-        {
-            Message message = await _messageRepository.GetById(messageId);
-
-            if (message != null)
-            {
-                var result = new SentMessage
-                {
-                    Id = message.Id,
-                    Subject = message.Subject,
-                    Body = message.Body,
-                    CreateDate = message.CreateDate
-                };
-
-                result.Correspondences = await GetCorrespondenceOfMessage(messageId);
-
-                return Ok(result);
-            }
-
-            return NotFound();
-        }
+        }        
 
         /// <summary>
         /// Gets result of correspondence by sent message
